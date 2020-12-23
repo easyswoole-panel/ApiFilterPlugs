ea57935688cd327ed3892cd7234f640dd5aa13bd
XueSi <1592328848@qq.com>
2020-12-24 02:17:57 +0800

    init

---
+++ esPlugsConfig.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: longhui
+ * Email: <1592328848@qq.com>
+ * Date: 2020/12/23
+ * Time: 22:40
+ */
+return [
+    'name'      => "IP 限流器",
+    'des'       => "IP 限流器插件",
+    'namespace' => "IpLimiterPlugs\\IpLimiter\\",
+    'version'   => '1.0',
+    'logo'      => "http://www.huanghui.xyz/img/avatar.png",
+    "home_view" => "",
+];
