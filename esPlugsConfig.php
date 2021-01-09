0d6b073eb8acda49b416bf39b5697dc8b1d1d3c9
宣言就是Siam <59419979@qq.com>
2021-01-09 22:04:40 +0800

    Update esPlugsConfig.php

--- esPlugsConfig.php
+++ esPlugsConfig.php
@@ -7,6 +7,6 @@ return [
     'logo'      => "https://s3.ax1x.com/2020/12/08/DzOz8S.jpg",
     'home' => [
         'type' => 0,
-        'url' => '/Siam/ApiFilterPlugs/index'
+        'url' => '/siam/api-filter-plugs/index'
     ]
 ];
